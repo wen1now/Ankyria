4140099380f967230765571e0b8f28c432e5d06f
wen1now <wen00@live.com.au>
2018-03-10 14:27:11 +1000

    People story typos changed

--- aims.js
+++ aims.js
@@ -30,50 +30,50 @@ l.aims.people_story = [
     'You walk over to investigate; their sentry sees you long before you arrive',
     'Although they appear uncivilised, they are undoubtedly a subspecies of <i>homo sapiens</i>',
     'The tallest of them - presumably their leader - steps forward and speaks clearly:',
-    '"I am BOB.',
-    'You ask, "Where are we? Why am I here"',
+    '"I am BOB."',
+    'You ask, "Where are we? Why am I here?"',
     'BOB goes back and a hushed discussion takes place.',
     'Finally he turns back to you:',
-    'You are on Ankyria - the planet of hope.',
-    'For some reason this isn\'t a surprise to you',
-    '"Here is our knowledge of what happened:"',
-    '<i>The planet Earth was unlivable. We had to evacuate. Our best scientists gathered together. They constructed a new species. This species was constructed to be almost impervious to anything: old age, dehydration, lack of food. These would just send them into a deep sleep. They sent individuals of this species out into the galaxy to find habitable planets. Somebody found this planet and contacted us. They also terraformed it using whatever technology they got. We got here thousands of years later. We never found out who it was.<\\i>'
+    '"You are on Ankyria - the planet of hope. Here is our knowledge of what happened:"',
+    '<i>The planet Earth was unlivable. We had to evacuate. Our best scientists gathered together. They constructed a new species. This species was constructed to be almost impervious to anything: old age, dehydration, lack of food. These would just send them into a deep sleep. They sent individuals of this species out into the galaxy to find habitable planets. Somebody found this planet and contacted us. They also terraformed it using whatever technology they got. We got here thousands of years later. We never found out who it was.</i>'
 ]
 
 l.endstory = function(){
     l.playing = true;
 }
 
-l.tellstory = function(somearray){
+l.tellstory = function(somearray,logmessage){
+    l.playing = false;
     var x=document.getElementById("maingame");
     x.innerHTML
     x.innerHTML="<button id='skipstory' onclick='l.endstory()'>Skip>>";
     var a = 0;
     var displaynext = function(somearray){
-        x.innerHTML += "<div style='opacity:0;' id='storytelling"+a+"'>"+somearray[a]+"</div>";
-        l.fadein(document.getElementById("storytelling"+a),a);
-        if (a>=1){document.getElementById("storytelling"+(a-1)).style.opacity = 1;}
-        a++;
+        if (!l.playing){
+            x.innerHTML += "<div style='opacity:0;' id='storytelling"+a+"'>"+somearray[a]+"</div>";
+            l.fadein(document.getElementById("storytelling"+a),a);
+            if (a>=1){document.getElementById("storytelling"+(a-1)).style.opacity = 1;}
+            a++;
+        }
         if ((a<somearray.length) && !(l.playing)){
-            y=setTimeout(displaynext,somearray[a-1].length*60,somearray);
+            y=setTimeout(displaynext,somearray[a-1].length*60+100,somearray);
         } else {
             document.getElementById("skipstory").innerHTML = "Continue>>";
             clearInterval(y)};
     };
     displaynext(somearray);
-}
-
-l.aims.initiate = function(){
-    l.playing = false;
-    l.tellstory(l.aims.init_story);
     var startgame = setInterval(function(){
         if (l.playing){
             l.tabs.list[0][0].settab();
             clearInterval(startgame);
             clearInterval(y);
-            l.log("I've landed. It wasn't as rough as I thought. However, my supplies are limited. I need to find some water <i>fast</i>, before what I have runs out (I'll need to drink 1 water every five hours). Hopefully I can survive here...");
+            if (logmessage!=undefined){l.log(logmessage)}
         }
-    },30);
+    },30,logmessage);
+}
+
+l.aims.initiate = function(){
+    l.tellstory(l.aims.init_story,"I've landed. It wasn't as rough as I thought. However, my supplies are limited. I need to find some water <i>fast</i>, before what I have runs out (I'll need to drink 1 water every five hours). Hopefully I can survive here...");
 }
 
 
